a459cab2a73d280bf980a2e0ed83334d40d4f9f4
dineshvardhan125 <dineshvardhan654@gmail.com>
2023-06-18 10:09:12 +0530

    june 18 23

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// my solution
+function makeUpperCase(str) {
+  return str.toUpperCase()
+}
---
+++ 8kyu/you-cant-code-under-presure.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// my Solution
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i+i
+}
+
