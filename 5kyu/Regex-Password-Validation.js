7cc01e79cf75425c82260519e447de1f704447ba
dineshvardhan125 <dineshvardhan654@gmail.com>
2023-07-06 21:45:56 +0530

    added

--- 5kyu/Regex-Password-Validation.js
+++ 5kyu/Regex-Password-Validation.js
@@ -2,7 +2,7 @@
 
 // // At least six characters long
 // // contains a lowercase letter
-// // contains an uppercase letter
+// // contains an uppercase letter.
 // // contains a digit
 // // only contains alphanumeric characters (note that '_' is not alphanumeric)
 
