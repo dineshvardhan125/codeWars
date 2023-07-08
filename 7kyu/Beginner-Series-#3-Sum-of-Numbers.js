4bc00f539523618f14d562a103d63910554f3449
dineshvardhan125 <dineshvardhan654@gmail.com>
2023-07-08 22:59:39 +0530

    5kyu

--- 7kyu/Beginner-Series-#3-Sum-of-Numbers.js
+++ 7kyu/Beginner-Series-#3-Sum-of-Numbers.js
@@ -1,6 +1,6 @@
 // Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
-// Note: a and b are not ordered!.
+// Note: a and b are not ordered!
 
 // Examples (a, b) --> output (explanation)
 // (1, 0) --> 1 (1 + 0 = 1)
