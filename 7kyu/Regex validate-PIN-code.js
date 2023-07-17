ce2579a706ee42cca6913c6e3694089430ce7ef4
dineshvardhan125 <dineshvardhan654@gmail.com>
2023-07-17 23:00:33 +0530

    catyears,dogyears

--- 7kyu/Regex validate-PIN-code.js
+++ 7kyu/Regex validate-PIN-code.js
@@ -3,7 +3,7 @@
 
 // If the function is passed a valid PIN string, return true, else return false.
 
-// Examples (Input --> Output)
+// Examples (Input --> Output),
 // "1234"   -->  true
 // "12345"  -->  false
 // "a234"   -->  false
