22307a1e08b23f80fef390ee59fe14fc937b6179
dineshvardhan125 <dineshvardhan654@gmail.com>
2023-07-20 18:30:41 +0530

    catyears,dogyears

--- 7kyu/sum-of-sequance.js
+++ 7kyu/sum-of-sequance.js
@@ -1,7 +1,7 @@
 // DESCRIPTION:
 // Your task is to write a function which returns the sum of a sequence of integers.
 
-// The sequence is defined by 3 non-negative values: begin, end, step.
+// The sequence is defined by 3 non-negative values: begin, end, step
 
 // If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
 
